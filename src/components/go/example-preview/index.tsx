0da9936b1fad3a1d7ca1580eb72a0b3974298581
sunyiteng <sunyiteng@bytedance.com>
2025-06-26 19:11:36 +0800

    refactor: remove @rspress/plugin-auto-nav-sidebar
    Change-Id: I793d958d3e3a3635f403797da3fb066cde2b4173

--- src/components/go/example-preview/index.tsx
+++ src/components/go/example-preview/index.tsx
@@ -5,9 +5,9 @@ import { ExampleContent } from './components';
 import { isAssetFileType } from './utils/example-data';
 import Callout from '../../Callout';
 import { SchemaOptionsData } from './hooks/use-switch-schema';
-import { withBase } from 'rspress/runtime';
+import { withBase } from '@rspress/core/runtime';
 
-const EXAMPLE_BASE_URL = '/lynx-examples';
+const EXAMPLE_BASE_URL = withBase('/lynx-examples');
 
 function useExampleBaseUrl() {
   return withBase(EXAMPLE_BASE_URL);
