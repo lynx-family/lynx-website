fe526829ccf18fa064710f6015eb1ea3f2e2d61b
Leron Liu <15890184+loongliu@users.noreply.github.com>
2025-11-07 14:42:38 +0800

    Update netlify.toml to make release/3.5 as latest release branch (#513)
    * Update netlify.toml to make release/3.5 as latest release branch
    
    Signed-off-by: Leron Liu <15890184+loongliu@users.noreply.github.com>
    
    * Update rspress.config.ts
    
    Signed-off-by: Leron Liu <15890184+loongliu@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Leron Liu <15890184+loongliu@users.noreply.github.com>

--- rspress.config.ts
+++ rspress.config.ts
@@ -93,7 +93,6 @@ export default defineConfig({
       'https://lf-lynx.tiktok-cdns.com/obj/lynx-artifacts-oss-sg/lynx-website/assets/lynx-dark-logo.svg',
     dark: 'https://lf-lynx.tiktok-cdns.com/obj/lynx-artifacts-oss-sg/lynx-website/assets/lynx-light-logo.svg',
   },
-  base: '/3.5',
   themeConfig: {
     locales: [
       {
