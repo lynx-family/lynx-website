b2896649c87b14c52b6839c837fa7a589b70e86d
Sherry <37186915+Sherry-hue@users.noreply.github.com>
2026-01-30 16:29:07 +0800

    Cherry-pick PR #764 && #769 to release/3.6 (#768)

--- src/components/go/example-preview/components/web-iframe.tsx
+++ src/components/go/example-preview/components/web-iframe.tsx
@@ -1,56 +1,51 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Spin, Typography } from '@douyinfe/semi-ui';
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
+import '@lynx-js/web-core/index.css';
+import '@lynx-js/web-elements/index.css';
+import type { LynxView } from '@lynx-js/web-core';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'lynx-view': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
 
 interface WebIframeProps {
   show: boolean;
   src: string;
 }
 
-const previewBaseUrl =
-  'https://www.unpkg.com/@lynx-js/web-explorer@0.0.15/index.html';
-let id = 1;
-
 export const WebIframe = ({ show, src }: WebIframeProps) => {
-  const [hasBeenVisible, setHasBeenVisible] = useState(false);
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [iframeId, setIframeId] = useState(0);
-  useEffect(() => {
-    if (show && !hasBeenVisible) {
-      setHasBeenVisible(true);
-      setIframeId(id++);
-    }
-  }, [show, hasBeenVisible]);
+  const lynxViewRef = useRef<LynxView>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (hasBeenVisible) {
-      if (iframeRef.current?.contentWindow) {
-        window?.addEventListener('message', (ev) => {
-          if (
-            ev.data?.method === 'iframeReady' &&
-            ev.data?.iframeId === String(iframeId)
-          ) {
-            setLoading(false);
-            iframeRef.current?.contentWindow?.postMessage(
-              {
-                method: 'setLynxViewUrl',
-                url: `${src}?fullscreen=true`,
-              },
-              '*',
-            );
-          }
-        });
-        iframeRef.current?.addEventListener('error', () => {
-          setLoading(false);
-          setError(true);
-        });
-      }
+    import('@lynx-js/web-core');
+    import('@lynx-js/web-elements/all');
+  }, []);
+
+  useLayoutEffect(() => {
+    if (lynxViewRef.current && src && show && containerRef.current) {
+      // @ts-ignore
+      lynxViewRef.current.browserConfig = {
+        pixelWidth: Math.round(
+          containerRef.current.clientWidth * window.devicePixelRatio,
+        ),
+        pixelHeight: Math.round(
+          containerRef.current.clientHeight * window.devicePixelRatio,
+        ),
+      };
+      lynxViewRef.current.url = src;
     }
-  }, [hasBeenVisible]);
+  }, [lynxViewRef.current, src, show]);
 
   return (
     <div
+      ref={containerRef}
       style={{
         display: show ? 'flex' : 'none',
         width: '100%',
@@ -60,47 +55,11 @@ export const WebIframe = ({ show, src }: WebIframeProps) => {
         position: 'relative',
       }}
     >
-      {hasBeenVisible && (
-        <iframe
-          id={String(iframeId)}
-          src={`${previewBaseUrl}?iframeId=${iframeId}`}
-          ref={iframeRef}
+      {show && src && (
+        <lynx-view
+          ref={lynxViewRef}
           style={{ width: '100%', height: '100%' }}
         />
-      )}
-      {loading && (
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-          }}
-        >
-          <Spin />
-        </div>
-      )}
-      {error && (
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-          }}
-        >
-          <Typography.Text type="tertiary">
-            Failed to load the preview, please try again later.
-          </Typography.Text>
-        </div>
       )}
     </div>
   );
