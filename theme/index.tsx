792a5789f22e0f6beaa2ad9b0156464073cd6936
WATER1350 <WATER1350@gmail.com>
2026-07-10 09:57:40 +0800

    update 3.9 index. (#1164)

--- theme/index.tsx
+++ theme/index.tsx
@@ -319,7 +319,7 @@ const Search = () => {
       docSearchProps={{
         appId: 'V4ET1OFZ5S', // cspell:disable-line
         apiKey: '15236c16e0f335c0cb2a67bc3ac06bcb', // cspell:disable-line
-        indexName: 'lynx_next',
+        indexName: 'lynx',
         searchParameters: {
           facetFilters: [`lang:${lang}`],
         },
